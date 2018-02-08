d14813a7d957cb33595d160056309d51c2a03d0d
Min Kim <mk4555@nyu.edu>
2018-02-08 15:50:10 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,7 +1,6 @@
 class Song < ActiveRecord::Base
   validates :title, presence: true
-  validates :release_year, presence: true, if: :released
-  validates :release_year, presence: false, if: !(:released)
+  validates :release_year, presence: true, if: :released == true
   validates :release_year, numericality: {less_than: 2019}
   validates :title, uniqueness: true
 end
