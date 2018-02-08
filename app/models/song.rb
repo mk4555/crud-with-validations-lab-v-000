59a27fd3c6606220a8ea4f3542f6e53144b5316d
Min Kim <mk4555@nyu.edu>
2018-02-08 16:29:20 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,4 +3,5 @@ class Song < ActiveRecord::Base
   validates :title, uniqueness: { scope: [:release_year, :artist_name], message: "Cannot be repeated by the same artist in the same year" }
   validates :release_year, presence: true, if: :released?
   validates :release_year, numericality: { less_than: Date.today.year }, if: :released?
+  validates_inclusion_of :released, :in => [true, false]
 end
