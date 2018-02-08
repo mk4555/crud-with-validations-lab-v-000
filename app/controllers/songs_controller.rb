6b98597c6fb7fab73a731130e05af49adddc3220
Min Kim <mk4555@nyu.edu>
2018-02-08 16:18:38 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -26,7 +26,7 @@ class SongsController < ApplicationController
 
   def update
     @song = Song.find(params[:id])
-     @song.update(songs_params)
+    @song.update(songs_params)
     if
       redirect_to song_path(@song)
     else
